830b40a6261035196c4923eb42e5017b4d874fde
lidiakim27 <lidiakim27@gmail.com>
2020-08-06 19:47:37 +0900

    초기상태로 복붙
    다시 시작!

--- app.py
+++ app.py
@@ -16,31 +16,15 @@ def home():
 ## API 역할을 하는 부분
 @app.route('/review', methods=['POST'])
 def write_review():
-    # title_receive로 클라이언트가 준 title 가져오기
-    title_receive = request.form['title_give']
-    # author_receive로 클라이언트가 준 author 가져오기
-    author_receive = request.form['author_give']
-    # review_receive로 클라이언트가 준 review 가져오기
-    review_receive = request.form['review_give']
-
-    # DB에 삽입할 review 만들기
-    review = {
-        'title': title_receive,
-        'author': author_receive,
-        'review': review_receive
-    }
-    # reviews에 review 저장하기
-    db.reviews.insert_one(review)
-    # 성공 여부 & 성공 메시지 반환
-    return jsonify({'result': 'success', 'msg': '리뷰가 성공적으로 작성되었습니다.'})
+    # 1. 클라이언트가 준 title, author, review 가져오기.
+    # 2. DB에 정보 삽입하기
+    # 3. 성공 여부 & 성공 메시지 반환하기
+    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
 
 
 @app.route('/review', methods=['GET'])
 def read_reviews():
-    # 1. DB에서 리뷰 정보 모두 가져오기
-    reviews = list(db.reviews.find({}, {'_id': 0}))
-    # 2. 성공 여부 & 리뷰 목록 반환하기
-    return jsonify({'result': 'success', 'reviews': reviews})
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
 
 
 if __name__ == '__main__':
